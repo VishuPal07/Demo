2855e52738e59f8bac885f68c37a4d7f6e28d8ea
VishuPal07 <vishu.pal@oodles.io>
2021-09-16 18:18:29 +0530

    comment

---
+++ src/Demo.java
@@ -0,0 +1,17 @@
+
+public class Demo {
+	public static void main(String[] args) {
+		int firstNumber = 10;
+		int secondNumber = 20;
+		int answer = addTwoNumber(firstNumber, secondNumber);
+		System.out.println(answer);
+
+	}
+
+	private static int addTwoNumber(int firstNumber, int secondNumber) {
+		// TODO Auto-generated method stub
+		int sum = firstNumber + secondNumber;
+		return sum;
+	}
+
+}
